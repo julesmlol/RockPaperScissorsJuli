07b6eddd7019b661508df36785347b9f9b854eba
julesmlol <161972469+julesmlol@users.noreply.github.com>
2024-07-04 09:45:20 +0300

    Update rock_paper_scissors.py

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -12,35 +12,35 @@ paper = 'Paper'
 scissors = 'Scissors'
 yes = 'yes'
 no = 'no'
-player_move = input("Choose [r]ock, [p]aper or [s]cissors: ")
-
-if player_move == "r":
-    player_move = rock
-elif player_move == "p":
-    player_move = paper
-elif player_move == "s":
-    player_move = scissors
-else: 
+while true:
+    player_move = input("Choose [r]ock, [p]aper or [s]cissors: ")
+    if player_move == "r":
+        player_move = rock
+    elif player_move == "p":
+        player_move = paper
+    elif player_move == "s":
+        player_move = scissors
+    else: 
     raise SystemExit("Invalid Input. Try again...")
+        continue
+    computer_move = ""
+    computer_random_number = random.randint(1, 3)
 
-computer_move = ""
-computer_random_number = random.randint(1, 3)
+    if computer_random_number == 2:
+        computer_move = rock
+        prRed("The computer chose rock")
+    elif computer_random_number == 3: 
+        computer_move = paper
+        prYellow("The computer chose paper")
+    else: 
+        computer_move = scissors
+        prCyan("The computer chose scissors")
+    if (player_move == rock and computer_move == scissors) or \
+            (player_move == paper and computer_move == rock) or \
+            (player_move == scissors and computer_move == paper):
+        prPurple("You win!")
 
-if computer_random_number == 2:
-    computer_move = rock
-    prRed("The computer chose rock")
-elif computer_random_number == 3: 
-    computer_move = paper
-    prYellow("The computer chose paper")
-else: 
-    computer_move = scissors
-    prCyan("The computer chose scissors")
-if (player_move == rock and computer_move == scissors) or \
-        (player_move == paper and computer_move == rock) or \
-        (player_move == scissors and computer_move == paper):
-    prPurple("You win!")
-
-elif player_move == computer_move:
-    prGreen("Draw!")
-else:
-    prRed("You lose!")
+    elif player_move == computer_move:
+        prGreen("Draw!")
+    else:
+        prRed("You lose!")
